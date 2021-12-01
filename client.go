8cc654b9509d4c94bb3fcb1e5bede24afbea83aa
sunl <sunlight0915@163.com>
2021-12-01 16:18:04 +0800

    [新增] 多模式选择

--- client.go
+++ client.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int //记录用户选择的聊天模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 
 	// 连接server
@@ -31,6 +33,50 @@ func NewClient(serverIp string, serverPort int) *Client {
 
 	// 返回对象
 	return client
+}
+
+// 菜单
+func (client *Client) menu() bool {
+	var flag int
+
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println("====请输入合法范围内的数据====")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+
+		}
+
+		// 根据不同的模式处理不同的业务
+		switch client.flag {
+		case 1:
+			// 公聊模式
+			fmt.Println("您选择的是公聊模式。。。")
+			break
+		case 2:
+			// 私聊模式
+			fmt.Println("您选择的是私聊模式。。。")
+			break
+		case 3:
+			// 重命名
+			fmt.Println("您选择的是重命名模式。。。")
+			break
+		}
+	}
 }
 
 var serverIp string
@@ -53,7 +99,7 @@ func main() {
 		return
 	}
 
-	fmt.Println("连接服务器成功")
+	fmt.Println("连接服务器成功......")
 
 	// 启动客户端业务
 	select {}
